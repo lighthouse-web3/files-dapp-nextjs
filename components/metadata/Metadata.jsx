27da620b2cb0ae76883fdfdc9127fc615fd03d6f
arpit <arpit@lighthouse.storage>
2022-09-14 01:04:45 +0530

    fb card fix

--- components/metadata/Metadata.jsx
+++ components/metadata/Metadata.jsx
@@ -1,41 +1,21 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React from "react";
 
 function Metadata({ title, description, url, image }) {
-  console.log("title", title);
-  console.log("title", description);
-  console.log("title", url);
-  console.log("title", image);
-  title = title ? title : "Lighthouse.Storage";
-  description = description
-    ? description
-    : "Permanent Storage Redefined | store files on decentralized network for lifetime at a fixed price";
-  url = url ? url : "https://www.lighthouse.storage/";
-  image = image ? image : "https://www.lighthouse.storage/logo.png";
-  console.log("title", title);
-  console.log("title", description);
-  console.log("title", url);
-  console.log("title", image);
+  const router = useRouter();
+  title ? title : "Lighthouse Storage";
+  description ? description : "Store Files Perpetually on IPFS and Filecoin";
+  image
+    ? image
+    : "https://www.lighthouse.storage/_next/image?url=%2Flogo.png&w=1920&q=75";
+
   return (
     <Head>
       {/* Primary Meta Tags  */}
       <title>{title}</title>
       <meta name="title" content={title} />
       <meta name="description" content={description} />
-
-      {/* <!-- Open Graph / Facebook --> */}
-      {/* <meta property="og:type" content="website" />
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} /> */}
-
-      {/* <!-- Twitter --> */}
-      {/* <meta data-rh="true"  name="twitter:card" content="summary_large_image" />
-      <meta data-rh="true"  name="twitter:url" content={url} />
-      <meta data-rh="true"  name="twitter:title" content={title} />
-      <meta data-rh="true"  name="twitter:description" content={description} />
-      <meta data-rh="true"  name="twitter:image" content={image} /> */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="color-scheme" content="dark" />
       <meta name="author" content="Lighthouse" />
@@ -44,7 +24,12 @@ function Metadata({ title, description, url, image }) {
       <meta name="twitter:creator" content="@LighthouseWeb3" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Lighthouse Storage" />
-      <meta property="og:url" content={url} />
+
+      <meta
+        property="og:url"
+        content={`https://www.lighthouse.storage${router?.asPath}`}
+        key="og-url"
+      />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
