d704e5e7986b171c10d3f8c43a6f791d2f6ee173
arpit <arpit@lighthouse.storage>
2022-09-13 22:00:04 +0530

    metadata fix

--- components/metadata/Metadata.jsx
+++ components/metadata/Metadata.jsx
@@ -2,12 +2,20 @@ import Head from "next/head";
 import React from "react";
 
 function Metadata({ title, description, url, image }) {
+  console.log("title", title);
+  console.log("title", description);
+  console.log("title", url);
+  console.log("title", image);
   title = title ? title : "Lighthouse.Storage";
   description = description
     ? description
     : "Permanent Storage Redefined | store files on decentralized network for lifetime at a fixed price";
   url = url ? url : "https://www.lighthouse.storage/";
   image = image ? image : "https://www.lighthouse.storage/logo.png";
+  console.log("title", title);
+  console.log("title", description);
+  console.log("title", url);
+  console.log("title", image);
   return (
     <Head>
       {/* Primary Meta Tags  */}
@@ -16,18 +24,35 @@ function Metadata({ title, description, url, image }) {
       <meta name="description" content={description} />
 
       {/* <!-- Open Graph / Facebook --> */}
-      <meta property="og:type" content="website" />
+      {/* <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={image} /> */}
 
       {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={url} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={image} />
+      {/* <meta data-rh="true"  name="twitter:card" content="summary_large_image" />
+      <meta data-rh="true"  name="twitter:url" content={url} />
+      <meta data-rh="true"  name="twitter:title" content={title} />
+      <meta data-rh="true"  name="twitter:description" content={description} />
+      <meta data-rh="true"  name="twitter:image" content={image} /> */}
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@LighthouseWeb3" />
+      <meta name="twitter:creator" content="@LighthouseWeb3" />
+      <meta
+        property="og:url"
+        content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/"
+      />
+      <meta property="og:title" content="A Twitter for My Sister" />
+      <meta
+        property="og:description"
+        content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling."
+      />
+      <meta
+        property="og:image"
+        content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
+      />
     </Head>
   );
 }
